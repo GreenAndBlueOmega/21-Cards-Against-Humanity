724cd5c1adcdc5bb07522e2e54ac09a51586ddfe
JessCole1492 <38048375+JessCole1492@users.noreply.github.com>
2018-06-05 17:25:58 -0600

    Add files via upload
    Added my current application folder, it is the newest one, and i added the folder where i have the pics that i am using for the main menu

---
+++ application/GUIController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.IOException;
+
+import controllers.Blackjack;
+import controllers.CardsAgainstHumanity;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUIController {
+	
+	public void cardsAgainstHumanityButton() {
+		
+	}
+	
+	public void blackjackButton() {
+		
+	}
+}
--- application/GUIMainMenu.java
+++ application/GUIMainMenu.java
@@ -1,30 +1,30 @@
 package application;
 
-import controllers.Blackjack;
-import controllers.CardsAgainstHumanity;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUIMainMenu extends Application {
-	
-	public void cardsAgainstHumanityClicked() {
-		CardsAgainstHumanity.cardsMainMenu();
-	}
-	
-	public void blackjackClicked() {
-		Blackjack.playBlackjack();
-	}
+	Stage window = new Stage();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("GUIMainMenu.fxml"));
-		primaryStage.setTitle("21 Cards Against Humanity");
-		primaryStage.setScene(new Scene(root, 1000, 800));
-		primaryStage.show();
-		primaryStage.setResizable(false);
+		window = primaryStage;
+		Parent menu = FXMLLoader.load(getClass().getResource("GUIMainMenu.fxml"));
+		
+		window.setTitle("21 Cards Against Humanity");
+		window.setScene(new Scene(menu));
+		window.setMaximized(true);
+		window.setResizable(true);
+		window.show();
 	}
 
 	public static void main(String[] args) {
